a288dcdbe1b819e43d4767c440b4b98741241269
Hieu Ho <hieu_ho@helloleotech.com>
2021-08-28 09:06:11 +0700

    Implement quiz type, lesson, quiz

---
+++ EMa.Data/ViewModel/ListLessonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EMa.Data.Entities.Common;
+using System;
+
+namespace EMa.Data.ViewModel
+{
+    public class ListLessonViewModel : ModelBase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string QuizName { get; set; }
+        public Guid QuizTypeId { get; set; }
+    }
+}
